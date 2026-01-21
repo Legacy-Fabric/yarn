ecab263306bda56891e2cf18b6deeeecfe7e03c3
Cat Core <arthur.koalas@free.fr>
2026-01-21 20:40:27 +0100

    Migrate workspace to keratin

--- yarn.py
+++ yarn.py
@@ -1,54 +1,96 @@
-#!/usr/bin/env python3
-
 import os
 import sys
+import subprocess
 
-sys.dont_write_bytecode = True
+MAPPINGS_DIR = 'mappings'
+GRADLE_TASKS = ['clean', 'enigma', 'enigmaWithoutUnpick', 'build', 'javadoc', 'javadocJar', 'checkMappings',
+                'mapMinecraftToIntermediary', 'mapMinecraftToNamed', 'decompileWithCfr', 'decompileWithVineflower',
+                'publish', 'loadMappings', 'insertMappings', 'saveMappingsDown', 'saveMappingsUp', 'saveMappings', 'startGraph']
+GRADLEW = 'gradlew' if os.name == 'nt' else './gradlew'
 
-VERSIONS = ['15w14a', '1.8.2', '1.8.3', '1.8.4', '1.8.5', '1.8.6', '1.8.7', '1.8.8', '1.8.9',
-            '1.9', '1.9.1', '1.9.2', '1.9.3', '1.9.4', '1.10', '1.10.1', '1.10.2',
-            '1.RV-Pre1', '1.11', '1.11.1', '1.11.2', '1.12',  '1.12.1', '1.12.2',
-            '1.13', '1.13.1', '1.13.2']
-GRADLE_PREFIX = "gradlew.bat" if os.name == "nt" else "./gradlew"
+# some jank to hide versions that are giving problems
+UNAVAILABLE_VERSIONS = []
+# shortcuts for versions with ugly ids
+VERSION_SHORTCUTS = {
+    '1.0': '1.0.0'
+}
 
 
 def main():
+    possible_versions = list(set(find_minecraft_versions()))
+    versions = []
+    tasks = []
+    options = []
+
     args = sys.argv
 
-    # Manual mode
-    if len(args) <= 1:
-        print("Enter the Minecraft version you want to use")
-        # print("Available versions: " + ", ".join(VERSIONS))
-        while True:
-            version = input("> ")
-            # if version.lower() in VERSIONS:
-            break
-            # print(".. Invalid version")
+    for i in range(1, len(args)):
+        arg = args[i]
+        parsed_arg = parse_minecraft_version(arg, possible_versions)
 
-        print("Enter the Gradle command you want to run")
-        print("('yarn' to open Enigma)")
-        command = input("> ")
-    # Argument mode
+        if parsed_arg:
+            for version in parsed_arg:
+                versions.append(version)
+        else:
+            if arg in GRADLE_TASKS:
+                tasks.append(arg)
+            elif arg.startswith('--') or arg.startswith('-D'):
+                options.append(arg)
+            else:
+                raise Exception('unrecognized arg ' + arg + '!')
+
+    if len(versions) == 0:
+        if 'MC_VERSION' in os.environ:
+            parsed_arg = parse_minecraft_version(os.environ['MC_VERSION'], possible_versions)
+
+            if parsed_arg:
+                for version in parsed_arg:
+                    versions.append(version)
+            else:
+                raise Exception('no minecraft version given!')
+        else:
+            raise Exception('no minecraft version given!')
+    if len(tasks) == 0:
+        raise Exception('no gradle tasks given!')
+
+    command = [GRADLEW]
+    command.extend(tasks)
+    command.extend(options)
+    command.append('--stacktrace')
+
+    if len(versions) == 1:
+        os.environ['MC_VERSION'] = versions[0]
     else:
-        if len(args) <= 2:
-            print("Invalid Syntax!")
-            print("Syntax:")
-            print("    python ./yarn.py {mc version} {gradle command}")
-            return
+        os.environ['MC_VERSIONS'] = ",".join(versions)
+    
+    subprocess.run(" ".join(command), shell=True, check=True)
 
-        version = args[1]
-        command = " ".join(args[2:])
+def find_minecraft_versions():
+    for filename in os.listdir("mappings"):
+        if filename.endswith(".tiny"):
+            yield filename[:-len(".tiny")]
+        elif filename.endswith(".tinydiff"):
+            if len(pair := filename[:-len(".tinydiff")].split("#")) == 2:
+                yield pair[-1]
 
-    exit_code = run_command_with_mcversion(version, GRADLE_PREFIX + " " + command + " unifyMappings")
 
-    if exit_code == 0:
-        exit(0)
-    else:
-        exit(1)
+def parse_minecraft_version(arg, possible_versions):
+    if arg in VERSION_SHORTCUTS.keys():
+        return parse_minecraft_version(VERSION_SHORTCUTS[arg], possible_versions)
+    versions = []
+    if arg in possible_versions:
+        versions.append(arg)
+    elif '&' not in arg:
+        for possible_version in possible_versions:
+            if '&' in possible_version:
+                for mc_version in possible_version.split('&'):
+                    if arg == mc_version:
+                        versions.append(possible_version)
+    for version in versions:
+        if version in UNAVAILABLE_VERSIONS:
+            raise Exception('version ' + version + ' is unavailable at the moment!')
+    return versions
 
-def run_command_with_mcversion(version: str, command: str) -> int:
-    os.environ["MC_VERSION"] = version
-    return os.system(command)
 
 if __name__ == '__main__':
     main()
